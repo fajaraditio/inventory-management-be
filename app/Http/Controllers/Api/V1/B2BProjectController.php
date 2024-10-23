6e41e357c0e1989e2df582c7c7cbdbc1deea23b6
Aditio <aditio.pluto@gmail.com>
2024-10-23 09:30:47 +0700

    Update b2b project controller

--- app/Http/Controllers/Api/V1/B2BProjectController.php
+++ app/Http/Controllers/Api/V1/B2BProjectController.php
@@ -28,7 +28,7 @@ class B2BProjectController extends Controller
 
         $projects = B2BProject::all();
 
-        $totalVariables = 8;
+        $totalVariables = count($validated) - 1; // subtract with 1 because lat, long only return geo similarity
         $similarities = [];
 
         foreach ($projects as $project) {
@@ -38,28 +38,35 @@ class B2BProjectController extends Controller
             $cityNameSimilarity         = CosineSimilarity::checkPercentage($project->city_name, $request->city_name);
             $districtSimilarity         = CosineSimilarity::checkPercentage($project->district_name, $request->district_name);
             $subDistrictSimilarity      = CosineSimilarity::checkPercentage($project->sub_district_name, $request->sub_district_name);
-            $totalAmountSimilarity      = CosineSimilarity::checkPercentage($project->total_amount, $request->total_amount);
             $geoSimilarity              = HaversineFormula::checkGeoSimilarity($project->latitude, $project->longitude, $request->latitude, $request->longitude);
 
-            $mergeSimilarity = ($projectNameSimilarity + $detailAddressSimilarity + $provinceNameSimilarity + $cityNameSimilarity + $districtSimilarity + $subDistrictSimilarity + $totalAmountSimilarity + $geoSimilarity) / $totalVariables;
+            $totalAmountSimilarity      = (min($project->total_amount, $request->total_amount) / max($project->total_amount, $request->total_amount)) * 100;
+            $mergeSimilarity            = ($projectNameSimilarity + $detailAddressSimilarity + $provinceNameSimilarity + $cityNameSimilarity + $districtSimilarity + $subDistrictSimilarity + $totalAmountSimilarity + $geoSimilarity) / $totalVariables;
 
-            $similarities[] = array_merge($project->toArray(), ['cosine_similarities' => [
-                'total_score'               => $mergeSimilarity,
-                'project_name_similarity'   => $projectNameSimilarity,
-                'detail_address_similarity' => $detailAddressSimilarity,
-                'province_name_similarity'  => $provinceNameSimilarity,
-                'city_name_similarity'      => $cityNameSimilarity,
-                'district_similarity'       => $districtSimilarity,
-                'sub_district_similarity'   => $subDistrictSimilarity,
-                'geo_similarity'            => $geoSimilarity,
-                'total_amount_similarity'   => $totalAmountSimilarity,
-            ]]);
+            $similarities[] = array_merge($project->toArray(), [
+                'total_similarity' => $mergeSimilarity,
+                'project_status' => $mergeSimilarity > 70 ? 'Existing Project' : 'New Project',
+                'similarities' => [
+                    'project_name_similarity'   => $projectNameSimilarity,
+                    'detail_address_similarity' => $detailAddressSimilarity,
+                    'province_name_similarity'  => $provinceNameSimilarity,
+                    'city_name_similarity'      => $cityNameSimilarity,
+                    'district_similarity'       => $districtSimilarity,
+                    'sub_district_similarity'   => $subDistrictSimilarity,
+                    'geo_similarity'            => $geoSimilarity,
+                    'total_amount_similarity'   => $totalAmountSimilarity,
+                ]
+            ]);
         }
+
+        $projects = collect($similarities);
+
+        $projects = $projects->sortByDesc('total_similarity');
 
         return response()->json([
             'status' => 'ok',
             'message' => 'Similarities of B2B Project Fetched',
-            'data' => $similarities,
+            'data' => $projects->values()->all(),
         ]);
     }
 }
