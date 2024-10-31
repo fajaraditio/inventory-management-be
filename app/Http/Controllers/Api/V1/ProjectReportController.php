f5c0ba816416cc599c0c55d039ff9c217ce4091f
Aditio <aditio.pluto@gmail.com>
2024-10-31 14:10:19 +0700

    Update project controller: - Adding fetch all - Adding fetch by Id

--- app/Http/Controllers/Api/V1/ProjectReportController.php
+++ app/Http/Controllers/Api/V1/ProjectReportController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProjectReport;
 use Illuminate\Http\Request;
 
 class ProjectReportController extends Controller
 {
-    public function fetch(Request $request) {
-        
+    public function fetch(Request $request)
+    {
+        $projectReports = new ProjectReport();
+
+        if ($request->search) {
+            $projectReports = $projectReports
+                ->where('salesman_name', 'like', '%' . $request->search . '%')
+                ->orWhere('title', 'like', '%' . $request->search . '%')
+                ->orWhere('content', 'like', '%' . $request->search . '%');
+        }
+
+        $projectReports = $projectReports->paginate();
+
+        return response()->json(
+            array_merge(
+                [
+                    'status'    => 'ok',
+                    'message'   => 'Project reports table fetched',
+                ],
+                $projectReports->toArray()
+            )
+        );
+    }
+
+    public function fetchId($id)
+    {
+        $projectReport = ProjectReport::find($id);
+
+        return response()->json(
+            [
+                'status'    => 'ok',
+                'message'   => 'Project reports table fetched',
+                'data' => $projectReport->toArray(),
+            ]
+        );
     }
 }
